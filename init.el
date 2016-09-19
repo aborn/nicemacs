924dfe4f2e7bcd0b57ea182987c1dab898c63299
guobao.jiang <loveaborn@foxmail.com>
2016-09-19 12:56:47 +0800

    modify elpa archives

--- init.el
+++ init.el
@@ -3,7 +3,14 @@
   (setq mac-option-modifier 'super)
   (setq mac-command-modifier 'meta))
 
-
 (add-to-list 'load-path "~/.emacs.d/hotkey")
-
 (require 'basic-key)
+
+(setq package-archives
+      '(
+        ("popkit" . "elpa.popkit.org/packages/")
+        ;;("melpa" . "melpa.org/packages/")
+        ;;("org"   . "orgmode.org/elpa/")
+        ;;("gnu"   . "elpa.gnu.org/packages/")
+        ))
+
