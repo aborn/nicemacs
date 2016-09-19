ba350ab3974f76d56ebba220b86afb317093548c
Aborn Jiang <loveaborn@foxmail.com>
2016-09-20 00:10:36 +0800

    add helm key

--- hotkey/basic-key.el
+++ hotkey/basic-key.el
@@ -2,9 +2,6 @@
 (global-set-key "\M-j" 'find-file)
 (global-set-key "\C-j" 'switch-to-buffer)
 
-(global-set-key (kbd "C-j") 'helm-buffers-list)
-(global-set-key (kbd "M-j") 'helm-find-files)
-
 (global-set-key (kbd "C-a") 'spacemacs/smart-move-beginning-of-line)
 ;; (global-set-key (kbd "C-w") 'spacemacs/backward-kill-word-or-region)
 (global-set-key [C-backspace] 'spacemacs/backward-kill-word-or-region)
@@ -12,5 +9,11 @@
 (global-set-key (kbd "C-'") 'nice/move-middle-of-line)
 (global-set-key (kbd "C-;") 'nice/move-forward-by-five)
 (global-set-key (kbd "C-:") 'nice/move-backward-by-five)
+
+;; helm 相关
+(global-set-key (kbd "C-j") 'helm-buffers-list)
+(global-set-key (kbd "M-j") 'helm-find-files)
+(global-set-key (kbd "M-x") 'helm-M-x)
+(global-set-key (kbd "M-y") 'helm-show-kill-ring)
 
 (provide 'basic-key)
