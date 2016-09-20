0517d9b7776065077aff1cfcafc01613e8d958f9
蒋国宝 <loveaborn@foxmail.com>
2016-09-20 20:57:52 +0800

    other-window

--- hotkey/basic-key.el
+++ hotkey/basic-key.el
@@ -1,6 +1,7 @@
 ;; (global-set-key "\C-o" 'other-window)
 (global-set-key "\M-j" 'find-file)
 (global-set-key "\C-j" 'switch-to-buffer)
+(global-set-key "\C-o" 'other-window)
 
 (global-set-key (kbd "C-a") 'spacemacs/smart-move-beginning-of-line)
 ;; (global-set-key (kbd "C-w") 'spacemacs/backward-kill-word-or-region)
