d9eab3c8ce666f42df05a3ffec330d101c448a90
guobao.jiang <loveaborn@foxmail.com>
2016-09-20 21:03:51 +0800

    update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,9 +4,8 @@ main() {
     if [ ! -d "$DIRECTORY" ]; then
         echo "git clone https://github.com/aborn/nicemacs.git ${DIRECTORY}"
         git clone https://github.com/aborn/nicemacs.git ${DIRECTORY}
-        echo "Add following code to your emacs init file:"
-        echo " (add-to-list 'load-path \"~/multi-term-plus\")"
-        echo " (require 'multi-term-config)"
+        echo "nicemacs installed success."
+        echo "version 0.1"
     else
         DATE=`date +%Y-%m-%d-%H-%M-%S`
         echo "$DATE"
