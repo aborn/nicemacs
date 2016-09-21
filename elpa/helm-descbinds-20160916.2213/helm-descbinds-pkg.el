6d489c2706944bf412dcf8795f02349f44c9bb20
guobao.jiang <loveaborn@foxmail.com>
2016-09-21 13:30:01 +0800

    helm-descbinds and use-package

---
+++ elpa/helm-descbinds-20160916.2213/helm-descbinds-pkg.el
@@ -0,0 +1,1 @@
+(define-package "helm-descbinds" "20160916.2213" "A convenient `describebindings' with `m'" '((helm "1.5")) 'url: "https://github.com/emacs-helm/helm-descbinds" :keywords '(" helm" " help"))
